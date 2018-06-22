34e21654280dcba547e0d87239754aad3881e5a8
唐理羚 <tangliling@zd.com>
2018-06-22 10:04:36 +0800

    1111111111

--- src/routes/user/Filter.js
+++ src/routes/user/Filter.js
@@ -3,18 +3,53 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import moment from 'moment'
 import { FilterItem } from 'components'
-import { Form, Button, Row, Col, DatePicker, Input, Cascader, Switch } from 'antd'
+import { Form, Button, Row, Col, DatePicker, Input, Cascader, Switch,Select } from 'antd'
 import city from '../../utils/city'
-
+import styles from './List.less'
+const Option = Select.Option;
 const { Search } = Input
 const { RangePicker } = DatePicker
+const FormItem = Form.Item
 
+const formItemLayout = {
+  labelCol: {
+    span:10,
+  },
+  wrapperCol: {
+    span: 14,
+  },
+  style:{
+    marginBottom: 0,
+    borderRadius:'20px',
+  }
+}
 const ColProps = {
   xs: 24,
-  sm: 12,
+  sm: 7,
   style: {
-    marginBottom: 16,
+    marginBottom: 10,
+    marginRight:10
   },
+}
+const  ColProps2={
+  xs: 24,
+  sm: 11,
+  style: {
+    marginBottom: 10,
+    marginRight:10
+  },
+}
+const formItemLayout2 = {
+  labelCol: {
+    span:6,
+  },
+  wrapperCol: {
+    span: 18,
+  },
+  style:{
+    marginBottom: 0,
+    borderRadius:'20px',
+  }
 }
 
 const TwoColProps = {
@@ -35,6 +70,7 @@ const Filter = ({
   },
 }) => {
   const handleFields = (fields) => {
+    console.log(fields)
     const { createTime } = fields
     if (createTime.length) {
       fields.createTime = [createTime[0].format('YYYY-MM-DD'), createTime[1].format('YYYY-MM-DD')]
@@ -43,7 +79,7 @@ const Filter = ({
   }
 
   const handleSubmit = () => {
-    let fields = getFieldsValue()
+    let fields = getFieldsValue();
     fields = handleFields(fields)
     onFilterChange(fields)
   }
@@ -82,30 +118,92 @@ const Filter = ({
   return (
   <div style={{padding:'20px',border:'1px #ddd solid',marginBottom:'35px'}}>
     <Row gutter={24}>
-      <Col {...ColProps} xl={{ span: 4 }} md={{ span: 8 }}>
-        {getFieldDecorator('name', { initialValue: name })(<Search placeholder="Search Name" onSearch={handleSubmit} />)}
+      <Col {...ColProps}>
+        <FormItem label="姓名" {...formItemLayout}>
+          {getFieldDecorator('usename', {
+          })(<Input />)}
+        </FormItem>
       </Col>
-      <Col {...ColProps} xl={{ span: 4 }} md={{ span: 8 }} id="addressCascader">
-        {getFieldDecorator('address', { initialValue: address })(<Cascader
-          style={{ width: '100%' }}
-          options={city}
-          placeholder="Please pick an address"
-          onChange={handleChange.bind(null, 'address')}
-          getPopupContainer={() => document.getElementById('addressCascader')}
-        />)}
+      <Col {...ColProps}>
+        <FormItem label="手机号"  {...formItemLayout}>
+          {getFieldDecorator('phone', {
+          })(<Input />)}
+        </FormItem>
       </Col>
-      <Col {...ColProps} xl={{ span: 6 }} md={{ span: 8 }} sm={{ span: 12 }} id="createTimeRangePicker">
-        <FilterItem label="Createtime">
-          {getFieldDecorator('createTime', { initialValue: initialCreateTime })(<RangePicker
+      <Col {...ColProps}>
+        <FormItem label="车牌"  {...formItemLayout}>
+          {getFieldDecorator('chepai', {
+          })(<Input />)}
+        </FormItem>
+      </Col>
+      <Col {...ColProps}>
+        <FormItem label="上年保险公司"  {...formItemLayout}>
+          {getFieldDecorator('company', {
+          })(<Select
+            showSearch
             style={{ width: '100%' }}
-            onChange={handleChange.bind(null, 'createTime')}
-            getCalendarContainer={() => {
-              return document.getElementById('createTimeRangePicker')
-            }}
-          />)}
-        </FilterItem>
+            placeholder="请选择"
+            dropdownStyle={{lineHeight:'25px'}}
+          >
+            <Option value="china">China</Option>
+            <Option value="use">U.S.A</Option>
+          </Select>)}
+        </FormItem>
+      </Col>
+      <Col {...ColProps}>
+        <FormItem label="投保保险公司"  {...formItemLayout}>
+          {getFieldDecorator('company2', {
+          })(<Select
+            showSearch
+            style={{ width: '100%' }}
+            placeholder="请选择"
+            dropdownStyle={{lineHeight:'25px'}}
+          >
+            <Option value="china">人保</Option>
+            <Option value="use">太保</Option>
+          </Select>)}
+        </FormItem>
+      </Col>
+      <Col {...ColProps}>
+        <FormItem label="预约级别"  {...formItemLayout}>
+          {getFieldDecorator('jibie', {
+          })(<Select
+            showSearch
+            style={{ width: '100%' }}
+            placeholder="请选择"
+            dropdownStyle={{lineHeight:'25px'}}
+          >
+            <Option value="china">A</Option>
+            <Option value="use">B</Option>
+          </Select>)}
+        </FormItem>
+      </Col>
+      <Col {...ColProps2}>
+        <FormItem label="初登日期"  {...formItemLayout2}>
+          {getFieldDecorator('beginDate', {
+          })(<RangePicker />)}
+        </FormItem>
+      </Col>
+      <Col {...ColProps2}>
+        <FormItem label="保险到期日"  {...formItemLayout2}>
+          {getFieldDecorator('endDate', {
+          })(<RangePicker />)}
+        </FormItem>
+      </Col>
+      <Col {...ColProps2}>
+        <FormItem label="名单发放日"  {...formItemLayout2}>
+          {getFieldDecorator('endDate', {
+          })(<RangePicker />)}
+        </FormItem>
+      </Col>
+      <Col {...ColProps2}>
+        <FormItem label="名单发放日"  {...formItemLayout2}>
+          {getFieldDecorator('endDate', {
+          })(<RangePicker />)}
+        </FormItem>
       </Col>
     </Row>
+
     <Row gutter={24}>
       <Col >
         <div style={{ display: 'flex', justifyContent: 'flex-end', flexWrap: 'wrap' ,}}>
