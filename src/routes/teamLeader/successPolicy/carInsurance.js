7ba963814cc7c068100e9295b15348cac98eb8c5
wut <1203969085@qq.com>
2018-06-27 10:29:03 +0800

    Merge branch 'master' of http://192.168.1.112:10080/tangliling/ZD-System
    # Conflicts:
    #	src/mock/menu.js
    #	src/routes/teamLeader/successPolicy/Modal.js
    #	src/routes/teamLeader/successPolicy/offer.less

--- src/routes/teamLeader/successPolicy/carInsurance.js
+++ src/routes/teamLeader/successPolicy/carInsurance.js
@@ -30,62 +30,40 @@ const ColProps = {
 const CarInsurance = () => {
 
   return (
-    <Form layout="horizontal">
+   <div>
       <div className={styles.header}> <img src="/ghef_03.png"/><span style={{marginLeft:'5px',fontSize:'15px',fontweight:'bold'}}>车险选项</span></div>
-      <Col offset={1}>
-      <Row style={{ marginTop: '3px' }}>
-          <FormItem {...formItemLayout}>
-            <div style={{width:'548px',height:'12px'}}
-                 className={styles.offerBoxb}>
-              <div className={classnames(styles.leftB,styles.ulList2)}>
-                <div>投保公司：</div>
+      <div>
+        <div className="useInfoRow">
+          <p>投保公司：</p> <p>太保</p><p>区域：</p><p>苏州 平安1</p>
+        </div>
 
-              </div>
-              <div  className={classnames(styles.rightB,styles.ulList2)}>
-                <div>区域：</div>
-              </div>
-            </div>
-          </FormItem>
-
-        <Divider dashed />
-      </Row>
-
-      <Row>
-        <Col span={2}>
-        <Col offset={1}>
-          <FormItem label="商业险" {...formItemLayout}>
-            <div style={{width:'548px',height:'120px',backgroundColor:'#F5FBFF'}}
-                 className={styles.offerBoxb}>
-              <div className={classnames(styles.leftB,styles.ulList2,styles.header)}>
-                <Col offset={2} >
-                  <div>商业险折扣：</div>
-                  <div>车辆损失险：</div>
-                  <div>不计免赔险：</div>
-                </Col>
-              </div>
-              <div  className={classnames(styles.rightB,styles.ulList2)}>
-                <div>风险保费：</div>
-                <div>第三者责任险：</div>
-              </div>
-            </div>
-          </FormItem>
-
-          <FormItem label="交强险" {...formItemLayout}>
-            <div style={{width:'548px',height:'38px',marginBottom:'20px',backgroundColor:'#F5FBFF'}}
-                 className={styles.offerBoxb}>
-              <div className={classnames(styles.leftB,styles.ulList2)}>
-                <Col offset={1}>
-                  <div>车船税：</div>
-                </Col>
+        <div className="useInfoRow">
+          商业险
+        </div>
+        <div style={{backgroundColor:'#F5FBFF'}}>
+        <div className="useInfoRow">
+          <p>商业险折扣：</p> <p>0.54</p><p>风险保费：</p><p>12211.00</p>
+        </div>
+        <div className="useInfoRow">
+          <p>车辆损失险：</p> <p>4.81万573.00</p><p>第三者责任险：</p><p>4.81万573.00</p>
+        </div>
+        <div className="useInfoRow">
+          <p>不计免赔险：</p> <p>1.96.00 车损/三责</p><p> </p><p> </p>
+        </div>
           </div>
-              </div>
-            </FormItem>
-        </Col>
-          </Col>
-      </Row>
-        </Col>
+        <div className="useInfoRow">
+          <li>交强险</li>
+        </div>
+        <div style={{backgroundColor:'#F5FBFF'}}>
+        <div className="useInfoRow">
+          <p>车船税：</p> <p>4.81万573.00</p><p>交强险：</p><p>4.81万573.00</p>
+        </div>
+          </div>
+        <div className="useInfoRow">
+        </div>
+      </div>
+     </div>
 
-   </Form>
   )
 }
 
--- src/routes/teamLeader/successPolicy/finalOffer.js
+++ src/routes/teamLeader/successPolicy/finalOffer.js
@@ -32,29 +32,23 @@ const FinalOffer = () => {
   return (
     <Form layout="horizontal">
       <div className={styles.header}> <img src="/ghef_03.png"/><span style={{marginLeft:'5px',fontSize:'15px',fontweight:'bold'}}>保单派送信息</span></div>
-      <Row>
-        <Col span={1}>
-            <FormItem{...formItemLayout}>
-              <div style={{width:'548px',height:'150px'}}
-                   className={styles.offerBoxb}>
-                <div className={classnames(styles.leftB,styles.ulList2)}>
-                  <Col offset={2}>
-                    <div>派送时间：</div>
-                    <div>联系方式：</div>
-                    <div>派送地址：</div>
-                    <div>备注：</div>
-                  </Col>
-                </div>
-                <div  className={classnames(styles.rightB,styles.ulList2)}>
-                  <div>收件人：</div>
-                  <div>收款方式：</div>
-                </div>
-              </div>
-            </FormItem>
-          </Col>
-      </Row>
+      <div>
+        <div className="useInfoRow">
+          <p>派送时间：</p> <p>2018-06-26</p><p>收件人：</p><p>刘媛媛</p>
+        </div>
+        <div className="useInfoRow">
+          <p>联系方式：</p> <p>13587589556</p><p>收款方式：</p><p>外出刷卡</p>
+        </div>
+        <div className="useInfoRow">
+          <div>派送地址：</div>
+          <div className="addresses">江苏省常熟市新北区孟河农副产品综合市场</div>
+        </div>
+        <div className="useInfoRow">
+          <div>备注：</div>
+          <div className="addresses">这里是备注备注备注备注备注备注</div>
+        </div>
 
-
+      </div>
     </Form>
   )
 }
