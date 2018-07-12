2eb7161b718b40e4b7003643b03ba4889d210edc
唐理羚 <tangliling@zd.com>
2018-07-12 18:27:26 +0800

    aaaaaaaa

--- src/routes/yewuyuan/dashboard/pie.js
+++ src/routes/yewuyuan/dashboard/pie.js
@@ -1,24 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Table, Modal } from 'antd'
-import classnames from 'classnames'
 import ReactEcharts from 'echarts-for-react';
 import { DropOption } from 'components'
 import { Link } from 'react-router-dom'
 import queryString from 'query-string'
 import styles from './pie.less'
-const { confirm } = Modal
 
 const Pie = ({
     data,
     title,
 }) => {
-  location.query = queryString.parse(location.search)
+  location.query = queryString.parse(location.search);
   const getOption={
     tooltip: {
       trigger: 'item',
       formatter: "{a} <br/>{b}: {c} ({d}%)"
     },
+    color:title!='今日派单'?['#8c9eff','#f4a21a']:['#8c9eff','#ec412b'],
     series: [
       {
         name:'访问来源',
@@ -48,8 +46,6 @@ const Pie = ({
     ]
   }
 
-
-
   return (
   <div className={styles.pieBox}>
     <p className="title">{title}</p>
@@ -60,7 +56,7 @@ const Pie = ({
     </div>
     <div className="numShow">
       {data.map((item,key)=>{
-        return <div><span className="dian"></span><span>{item.name}</span><span className="value">{item.value}</span></div>
+        return <div key={key}><span className="dian"></span><span>{item.name}</span><span className="value">{item.value}</span></div>
       })}
     </div>
   </div>
